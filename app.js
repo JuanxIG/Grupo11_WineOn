7b367fc48d2ec8f4715524d5dd3457bc87965bb9
DamO1103 <damianjodriozola@gmail.com>
2021-10-30 16:30:29 -0300

    app hecho

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const publicPath = path.resolve(__dirname, "./public");
+
+app.use(express.static(publicPath));
+
+app.listen(3000, () => {
+    console.log("Servidor corriendo....");
+    console.log(__dirname);
+})
+
+app.get("/home", (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/index.html"))});
+
+app.get("/productos", (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/productDetail.html"))});
+
+app.get("/carrito", (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/productCart.html"))});
+
+app.get("/registro", (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/register.html"))});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname,"/views/login.html"))});
