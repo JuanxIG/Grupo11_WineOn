ac5f5eb10892c33b19f54d5afc1046b8ac170450
soldelv <joanadelvalle@hotmail.com.ar>
2021-12-06 02:06:28 -0300

    se corrigen las rutas

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -6,17 +6,17 @@ const productController = require("../controllers/productController")
 router.get("/", productController.products)
 
 //ruta que muestra el formulario para agregar un producto y procesar el formulario
-router.get("/productos/agregar", productController.showAdd)
-router.post("/productos/agregar/:id", productController.addProduct)
+router.get("/agregar", productController.showAdd)
+router.post("/agregar/:id", productController.addProduct)
 
 //ruta que lleva al detalle de un producto
 router.get("/detail/:id", productController.detail);
 
 //ruta que muestra el formulario de edicion de un producto y procesar el formulario
-router.get("/productos/editar/:id", productController.formProduct)
-router.put("/productos/editar/:id", productController.editProduct)
+router.get("/editar/:id", productController.formProduct)
+router.put("/editar/:id", productController.editProduct)
 
 //ruta que elimina un producto
-router.delete("/productos/editar/:id", productController.deleteProduct)
+router.delete("/editar/:id", productController.deleteProduct)
 
 module.exports = router;
