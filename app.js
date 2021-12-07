df7ed4b74744806e1693a01afd13c1d11d16cf1b
soldelv <joanadelvalle@hotmail.com.ar>
2021-12-07 19:53:06 -0300

    se completa app.js

--- app.js
+++ app.js
@@ -5,9 +5,18 @@ const mainRouter = require("./src/routes/mainRouter");
 const productRouter = require("./src/routes/productRouter");
 const methodOverride =  require('method-override'); // Pasar poder usar los métodos PUT y DELETE
 
-app.use(express.static("public"));
+//app.use(express.static("public"));
 
-app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, '../public'))); // Necesario para los archivos estáticos en el folder /public
+app.use(express.urlencoded({ extended: false }));
+app.use(logger('dev'));
+app.use(express.json());
+app.use(cookieParser());
+app.use(methodOverride('_method')); 
+
+// ************ Template Engine - (don't touch) ************
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views')); // Define la ubicación de la carpeta de las Vistas
 
 app.listen(3500, () => {
     console.log("Servidor corriendo....");
