50445c114213d56ac15381f1dd8e57899cdae92d
Juan Ignacio <juanimen34@gmail.com>
2022-03-17 15:07:58 -0300

    carrito casi listo

--- public/js/carrito.js
+++ public/js/carrito.js
@@ -4,20 +4,19 @@ window.addEventListener("load",function (){
         div.innerHTML += "<h2>No hay productos agregados </h2>";
     } else {
         let carrito = JSON.parse(localStorage.carrito);
-        console.log(carrito);
         for (let i = 0; i < carrito.length; i++) {
             let vacio = document.querySelector("#vacio")
             let producto = carrito[i];
             let htmlcarrito =`  <article class="article-main"> 
                     <div id="main">
                         <img src=${producto.imagen} alt="" width="50" height="50" class="imagen"/>
-                        <div>${producto.nombre}</div>
+                        <div>${producto.tituloProducto}</div>
                         <i class="fas fa-trash"></i>
                     </div>
                     <div class="sub-main">
                         <div>PRECIO</div>
                         <div>CANTIDAD</div>
-                        <div>SUBTOTAL</div>
+                        <div>DESCUENTO</div>
                     </div>
                     <div class="detalles">
                         <div>${producto.precio}</div>
@@ -26,7 +25,7 @@ window.addEventListener("load",function (){
                             <div>${producto.cantidad}</div>
                             <div>-</div>
                         </div>
-                        <div class="subtotal-price">$$$$</div>
+                        <div class="subtotal-price">${producto.descuento}</div>
                     </div>
                     
                     <div class="linea">
@@ -41,12 +40,23 @@ window.addEventListener("load",function (){
       let contenido2 = `0`
       total.innerHTML += contenido2
     } else {
-      let contenido2 = `${toThousand(totalCarrito)}`
+      let contenido2 = `${totalCarrito}`
       total.innerHTML += contenido2
     }
+
+    let cantidad = document.querySelector("#precio-subtotal")
+    let cantidadTotal = localStorage.cantidadTotal
+    if(typeof localStorage.cantidadTotal == 'undefined'){
+      let contenido2 = `0`
+      cantidad.innerHTML += contenido2
+    } else {
+      let contenido2 = `${cantidadTotal}`
+      cantidad.innerHTML += contenido2
+    }
+
   });
   
-    function borrarItem(id) {
+    /*function borrarItem(id) {
         let carrito = JSON.parse(localStorage.carrito);
         carrito = carrito.filter((producto, i) => {
         return i !== id;
@@ -54,13 +64,13 @@ window.addEventListener("load",function (){
   
     localStorage.setItem("carrito", JSON.stringify(carrito));
     location.reload();
-  }
+  }*/
   
-    /*let botonBorrar = document.querySelector("#botonBorrar");
+    let botonBorrar = document.querySelector("#borrarTodo");
     botonBorrar.addEventListener('click', function(e) {
         e.preventDefault();
         localStorage.clear();
         alert('has vaciado el carrito');
         location.reload();
-  })*/
+  })
 
